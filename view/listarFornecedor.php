e78549b2f8da8d62576eaf0b81cb7b6b2af496e8
Gabriel Fontoura Rosa <182210123@senacinformatica.br>
2024-03-27 09:38:58 -0300

    att 27/03

--- view/listarFornecedor.php
+++ view/listarFornecedor.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../view/estilo.css">
-    <title>Listagem de clientes</title>
+    <title>Listagem de fornecedores</title>
 </head>
 
 <body>
